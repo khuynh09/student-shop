c809a1100de9306923bd7f160883e868df07b174
Maireen Espiritu <imaireen@gmail.com>
2021-05-31 21:15:48 -0700

    created account page

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
+import React from "react";
 import styled from "styled-components";
 import profileImage from "../assets/profile.jpeg";
 import listingImage from "../assets/square.png";
